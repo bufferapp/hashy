118549c538cb37dbf5620c22b9f91b51c59805ee
David Gasquez <davidgasquez@gmail.com>
2020-05-29 13:03:29 +0200

    Move data

--- api/train_embedding.py
+++ api/train_embedding.py
@@ -2,11 +2,11 @@ from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 
 w2vmodel = Word2Vec(
-    corpus_file="data/updates_hashtags.csv",
-    size=400,
-    window=8,
-    iter=30,
-    min_count=30,
+    corpus_file="data/updates_hashtags_full.csv",
+    size=100,
+    window=5,
+    iter=10,
+    min_count=50,
     workers=16,
 )
 
